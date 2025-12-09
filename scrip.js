5ace37a033ada1a0d5ab56e91ce124c463530d91
Ketlyn Maia <ketlynmaia.souza@gmail.com>
2025-12-09 13:06:51 -0300

    meu primeiro commit

---
+++ scrip.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  //if (html.classList.contains('light')) {
+  //html.classList.remove('light')
+  //} else {
+  //  html.classList.add('light')
+  //} ou usar o atalho abaixo:
+  
+  html.classList.toggle('light')
+
+
+}
